2a393f0ebe893d61f637c2c1ed759dd303337bc4
Danislav Konyarski <unstopable46@gmail.com>
2023-01-20 09:39:45 +0200

    new task

--- DataTypesAndVariables/src/DataTypesAndVariablesLab/T1ConvertMetersToKilometers.java
+++ DataTypesAndVariables/src/DataTypesAndVariablesLab/T1ConvertMetersToKilometers.java
@@ -1,4 +1,12 @@
 package DataTypesAndVariablesLab;
 
+import java.util.Scanner;
+
 public class T1ConvertMetersToKilometers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int metres = Integer.parseInt(scanner.nextLine());
+        double km = metres / 1000.0;
+        System.out.printf("%.2f",km);
+    }
 }
