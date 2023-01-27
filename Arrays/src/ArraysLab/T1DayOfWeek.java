172bbe770e6fb5f40e4a9c817197aeb6aed793d0
Danislav Konyarski <unstopable46@gmail.com>
2023-01-27 15:00:12 +0200

    Array task 1

--- Arrays/src/ArraysLab/T1DayOfWeek.java
+++ Arrays/src/ArraysLab/T1DayOfWeek.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class T1DayOfWeek {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int input = Integer.parseInt(scanner.nextLine());
+        String[] dayWeekArr = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        if (input>=1&&input<=7){
+            System.out.println(dayWeekArr[input-1]);
+        }else {
+            System.out.println("Invalid day!");
+        }
     }
 }
