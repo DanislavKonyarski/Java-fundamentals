ecc0c1f2d5b021e255225b159ed49ee0d30c669a
Danislav Konyarski <unstopable46@gmail.com>
2023-01-28 10:26:28 +0200

    arrays exercise

---
+++ Arrays/src/ArraysExercise/T1Train.java
@@ -0,0 +1,10 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class T1Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
