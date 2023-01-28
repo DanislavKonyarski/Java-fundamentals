918bab13e9511c0842d911d0248cf7438285018a
Danislav Konyarski <unstopable46@gmail.com>
2023-01-28 10:55:38 +0200

    new task

--- Arrays/src/ArraysExercise/T1Train.java
+++ Arrays/src/ArraysExercise/T1Train.java
@@ -1,10 +1,23 @@
 package ArraysExercise;
 
+import java.sql.Array;
 import java.util.Scanner;
 
 public class T1Train {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int countWagons = Integer.parseInt(scanner.nextLine());
+        int[] peopleArr = new int[countWagons];
+        int sum = 0;
+        for (int i = 0; i < countWagons; i++) {
+            int people = Integer.parseInt(scanner.nextLine());
+            sum += people;
+            peopleArr[i]=people;
+        }
+        for (int item : peopleArr) {
+            System.out.print(item+" ");
+        }
+        System.out.println();
+        System.out.println(sum);
     }
 }
