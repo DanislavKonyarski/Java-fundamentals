2353c9f9ad24c4220e28bc22017a61e071dbe740
Danislav Konyarski <unstopable46@gmail.com>
2023-02-04 17:08:18 +0200

    new task

---
+++ MethodsLab/src/T1SignOfIntegerNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class T1SignOfIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        signInteger(number);
+    }
+    public static void signInteger (int number){
+        if (number>0){
+            System.out.printf("The number %d is positive.",number);
+        } else if (number==0) {
+            System.out.println("The number 0 is zero.");
+        }else {
+            System.out.printf("The number %d is negative.",number);
+        }
+    }
+}
