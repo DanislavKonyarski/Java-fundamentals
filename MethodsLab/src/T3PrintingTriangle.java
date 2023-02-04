f1a6ff3e788766950f27e2768ef20f1bae271744
Danislav Konyarski <unstopable46@gmail.com>
2023-02-04 18:38:21 +0200

    new task

---
+++ MethodsLab/src/T3PrintingTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class T3PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        prime(number);
+    }
+    public static void prime (int number){
+        for (int i = 1; i < number; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for (int i = number; i >= 1; i--) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
