7b78fb38682d35ef6ef8ca6a998277c3a64268a3
Danislav Konyarski <unstopable46@gmail.com>
2023-02-05 16:25:04 +0200

    new task

---
+++ MethodsLab/src/T8MathPower.java
@@ -0,0 +1,19 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class T8MathPower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number = Double.parseDouble(scanner.nextLine());
+        double power = Double.parseDouble(scanner.nextLine());
+        double matPower = matPow(number,power);
+        System.out.println(new DecimalFormat("0.#######").format(matPower));
+    }
+    public static double matPow (double number, double pow){
+        double matPow = 1;
+        for (int i = 0; i < pow; i++) {
+            matPow *= number;
+        }
+        return matPow;
+    }
+}
