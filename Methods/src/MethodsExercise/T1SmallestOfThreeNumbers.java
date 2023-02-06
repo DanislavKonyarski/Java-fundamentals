9c120dee383e6422cd7c76948a28e3ecf252d278
Danislav Konyarski <unstopable46@gmail.com>
2023-02-06 17:27:57 +0200

    new module

--- Methods/src/MethodsExercise/T1SmallestOfThreeNumbers.java
+++ Methods/src/MethodsExercise/T1SmallestOfThreeNumbers.java
@@ -1,7 +1,10 @@
 package MethodsExercise;
 
+import java.util.Scanner;
+
 public class T1SmallestOfThreeNumbers {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
     }
 }
