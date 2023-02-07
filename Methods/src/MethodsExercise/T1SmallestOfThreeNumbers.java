31be1a8800da3a0d8ed278b1b1be540eb1de1db2
Danislav Konyarski <unstopable46@gmail.com>
2023-02-07 16:39:48 +0200

    new task

--- Methods/src/MethodsExercise/T1SmallestOfThreeNumbers.java
+++ Methods/src/MethodsExercise/T1SmallestOfThreeNumbers.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class T1SmallestOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numFirst = Integer.parseInt(scanner.nextLine());
+        int numTwo = Integer.parseInt(scanner.nextLine());
+        int numTree = Integer.parseInt(scanner.nextLine());
+        smallestNumber(numFirst,numTwo,numTree);
+    }
+    public static void smallestNumber (int numOne, int numTwo,int numTree){
+        if (numOne>=numTwo){
+            if (numOne>=numTree){
+                System.out.println(numTree);
+            }else {
+                System.out.println(numTwo);
+            }
+        }else {
+            System.out.println(numOne);
+        }
     }
 }
