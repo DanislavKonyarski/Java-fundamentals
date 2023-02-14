01ecbdc81a91cd4139726d8e00728b6594719d48
Danislav Konyarski <unstopable46@gmail.com>
2023-02-14 21:05:01 +0200

    new task

---
+++ List/src/ListsExercise/T1Train.java
@@ -0,0 +1,10 @@
+package ListsExercise;
+
+import java.util.Scanner;
+
+public class T1Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
