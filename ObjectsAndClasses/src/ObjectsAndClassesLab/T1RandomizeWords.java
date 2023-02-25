ff892da98c2da7e1aae5084bb8f2833d3c6e26c0
Danislav Konyarski <unstopable46@gmail.com>
2023-02-25 16:31:38 +0200

    new task

--- ObjectsAndClasses/src/ObjectsAndClassesLab/T1RandomizeWords.java
+++ ObjectsAndClasses/src/ObjectsAndClassesLab/T1RandomizeWords.java
@@ -1,7 +1,26 @@
 package ObjectsAndClassesLab;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class T1RandomizeWords {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        List<String> inputLine = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        for (int i = 0; i < inputLine.size()-1 ; i++) {
+            int index = random.nextInt(inputLine.size()-1);
+            if (!inputLine.get(i).equals(inputLine.get(index))){
+                String workString = inputLine.get(index);
+                inputLine.set(index,inputLine.get(i));
+                inputLine.set(i,workString);
+            }
+        }
+        for (String s : inputLine) {
+            System.out.println(s);
+        }
     }
 }
