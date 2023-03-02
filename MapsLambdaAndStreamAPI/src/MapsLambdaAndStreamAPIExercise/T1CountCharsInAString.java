26434b9b9fbbe599540007dadbea843fe16e4502
Danislav Konyarski <unstopable46@gmail.com>
2023-03-02 17:52:49 +0200

    new task

--- MapsLambdaAndStreamAPI/src/MapsLambdaAndStreamAPIExercise/T1CountCharsInAString.java
+++ MapsLambdaAndStreamAPI/src/MapsLambdaAndStreamAPIExercise/T1CountCharsInAString.java
@@ -1,10 +1,28 @@
 package MapsLambdaAndStreamAPIExercise;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class T1CountCharsInAString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] inputLineArr = scanner.nextLine().split(" ");
+        Map<Character,Integer> symbolCountMap = new LinkedHashMap<>();
+        for (int i = 0; i < inputLineArr.length; i++) {
+            String currentWord = inputLineArr[i];
+            for (int j = 0; j < currentWord.length(); j++) {
+                char symbol = currentWord.charAt(j);
+                if (symbolCountMap.containsKey(symbol)){
+                    symbolCountMap.put(symbol,symbolCountMap.get(symbol)+1);
+                }else {
+                    symbolCountMap.put(symbol,1);
+                }
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : symbolCountMap.entrySet()) {
+            System.out.printf("%s -> %d%n",entry.getKey(),entry.getValue());
+        }
 
     }
 }
