24657274a839bb133a8c60c23a0b4644cca86902
Danislav Konyarski <unstopable46@gmail.com>
2023-03-02 16:04:15 +0200

    new task

---
+++ MapsLambdaAndStreamAPI/src/MapsLambdaAndStreamAPILab/T4WordFilter.java
@@ -0,0 +1,12 @@
+package MapsLambdaAndStreamAPILab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T4WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] inputLine = Arrays.stream(scanner.nextLine().split(" ")).filter(e -> e.length()%2==0).toArray(String[]::new);
+        
+    }
+}
