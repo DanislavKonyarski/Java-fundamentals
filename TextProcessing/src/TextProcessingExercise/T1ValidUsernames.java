fe2fd2e7ad4eb240130768d51c76a1faadfdf073
Danislav Konyarski <unstopable46@gmail.com>
2023-03-08 15:49:35 +0200

    new task

---
+++ TextProcessing/src/TextProcessingExercise/T1ValidUsernames.java
@@ -0,0 +1,9 @@
+package TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class T1ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
