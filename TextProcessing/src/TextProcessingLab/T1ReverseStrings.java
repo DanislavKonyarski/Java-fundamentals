6bf4d62c31f94d37d8310ba64cb55fb8f4b53a7e
Danislav Konyarski <unstopable46@gmail.com>
2023-03-08 11:24:01 +0200

    new module

---
+++ TextProcessing/src/TextProcessingLab/T1ReverseStrings.java
@@ -0,0 +1,10 @@
+package TextProcessingLab;
+
+import java.util.Scanner;
+
+public class T1ReverseStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
