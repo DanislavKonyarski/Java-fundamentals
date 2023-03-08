f2bd48c26510b10df5b31bed664aa034232b5fcf
Danislav Konyarski <unstopable46@gmail.com>
2023-03-08 11:53:17 +0200

    new task

--- TextProcessing/src/TextProcessingLab/T1ReverseStrings.java
+++ TextProcessing/src/TextProcessingLab/T1ReverseStrings.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class T1ReverseStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        String input = scanner.nextLine();
+        while (!input.equals("end")){
+            StringBuilder inputBuilder = new StringBuilder(input);
+            System.out.println(input+" = "+inputBuilder.reverse());
+            input = scanner.nextLine();
+        }
     }
 }
