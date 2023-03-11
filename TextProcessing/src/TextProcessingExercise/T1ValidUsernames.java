21a4b4ca68f1c4867bdd909a0e2eb03f1367b989
Danislav Konyarski <unstopable46@gmail.com>
2023-03-11 13:05:43 +0200

    new task

--- TextProcessing/src/TextProcessingExercise/T1ValidUsernames.java
+++ TextProcessing/src/TextProcessingExercise/T1ValidUsernames.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class T1ValidUsernames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] inputLineArr = scanner.nextLine().split(", ");
+        for (int i = 0; i < inputLineArr.length; i++) {
+            boolean flag = false;
+            String word = inputLineArr[i];
+            if (word.length()>=3&&word.length()<=16){
+                for (int j = 0; j < word.length(); j++) {
+                    char symbol = word.charAt(j);
+                    if (Character.isLetterOrDigit(symbol)||symbol=='_'||symbol=='-'){
+                        flag = true;
+                    }else {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (flag){
+                System.out.println(word);
+            }
+        }
     }
 }
