3a5ec14fc95c64d0142157e1d8f0d20e2719776b
Danislav Konyarski <unstopable46@gmail.com>
2023-03-17 15:38:53 +0200

    new package

---
+++ RegularExpressions/src/RegularExpressionsExercises/T1Furniture.java
@@ -0,0 +1,10 @@
+package RegularExpressionsExercises;
+
+import java.util.Scanner;
+
+public class T1Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
