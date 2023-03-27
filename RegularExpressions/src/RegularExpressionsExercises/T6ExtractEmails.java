297f2995b5b2de88741774b4f431017f81696644
Danislav Konyarski <unstopable46@gmail.com>
2023-03-27 16:06:10 +0300

    new task

--- RegularExpressions/src/RegularExpressionsExercises/T6ExtractEmails.java
+++ RegularExpressions/src/RegularExpressionsExercises/T6ExtractEmails.java
@@ -1,10 +1,17 @@
 package RegularExpressionsExercises;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class T6ExtractEmails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input = scanner.nextLine();
+        Pattern pattern = Pattern.compile("(?<userName>[A-Za-z0-9]+[.\\-_]?[A-Za-z0-9]*)@(?<host>[A-Za-z]+\\-?[A-Za-z]+(\\.[A-Za-z]\\-?[A-Za-z]*)+)");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            System.out.printf("%s%n",matcher.group());
+        }
     }
 }
